2759a37b4a536d553f04c6cc7d65d2a9c7994be7
Anik Dey <anikdey975197@gmail.com>
2021-11-21 00:31:52 +0600

    mastering update

--- resources/views/components/frontend/css.blade.php
+++ resources/views/components/frontend/css.blade.php
@@ -1,8 +1,3 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>Women &ndash; Belle Multipurpose Bootstrap 4 Template</title>
-<meta name="description" content="description">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Favicon -->
 <link rel="shortcut icon" href="{{ asset('frontend/assets/images/favicon.png') }}" />
 <!-- Plugins CSS -->
