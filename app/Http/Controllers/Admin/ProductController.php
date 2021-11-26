54cfb910b8a316dd85f9cadf821182f7b7e4f5c5
Anik Dey <anikdey975197@gmail.com>
2021-11-26 19:51:46 +0600

    products

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,13 +5,61 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Carbon;
+use Image;
 
 class ProductController extends Controller
 {
-    Public function createProduct()
+    Public function createProduct(Request $request)
     {
         $categories=Category::orderby('name', 'asc')->get();
-        return view('admin.create-product',compact('categories'));
+        if($request->method()=='GET')
+        {
+            return view('admin.create-product',compact('categories'));
+        }
+
+        if($request->method()=='POST')
+        {
+            //dd($request->all());
+            $validator = $request->validate([
+                'product_name'      => 'required',
+                'product_des'       => 'required',
+                'product_quantity'  => 'required',
+                'category_id'       => 'required',
+                'product_price'     => 'required|numeric',
+                'option'            => 'required',
+                'product_image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+
+            ]);
+            $randomNumber = random_int(100000, 999999);
+
+            $product_image = $request->file('product_image');
+            $input['product_image'] = time().'.'.$product_image->extension();
+            $destinationPath = public_path('/products');
+            $img = Image::make($product_image->path());
+            $img->resize(1072, 1500, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save($destinationPath.'/'.$input['product_image']);
+            $destinationPath = public_path('/products');
+            $image->move($destinationPath, $input['product_image']);
+
+            $product = new Product();
+            $product->name = $request->product_name;
+            $product->code =  $randomNumber;
+            $product->des = $request->product_des;
+            $product->quality = $request->product_quality;
+            $product->price = $request->product_price;
+            $product->image = "/public/products/" . $input['product_image'];
+            $product->status = $request->option;
+            $product->category_id = $request->category_id;
+            $product->created_at = Carbon::now();
+            $product->updated_at = Carbon::now();
+
+            $product->save();
+            return redirect()->back()->with('success', 'Product has been created successfully.');
+        }
+
     }
 }
--- resources/views/admin/create-product.blade.php
+++ resources/views/admin/create-product.blade.php
@@ -11,24 +11,31 @@
                     </div>
                     <div class="row tm-edit-product-row">
                         <div class="col-xl-6 col-lg-6 col-md-12">
-                            <form action="" class="tm-edit-product-form">
+                            <form action="" role="form" method="POST" class="tm-edit-product-form"
+                                enctype="multipart/form-data">
+                                @csrf
                                 <div class="form-group mb-3">
                                     <label for="name">Product Name
                                     </label>
-                                    <input id="name" name="name" type="text" class="form-control validate" required />
+                                    <input id="name" name="product_name" value="{{ old('product_name') }}" type="text"
+                                        placeholder="Product Name" class="form-control validate" required />
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="description">Description</label>
-                                    <textarea class="form-control validate" rows="3" required></textarea>
+                                    <textarea class="form-control validate" rows="3" name="product_des"
+                                        placeholder="Product Description" value="{{ old('product_des') }}"
+                                        required></textarea>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="name">Product Quantity
                                     </label>
-                                    <input id="name" name="name" type="text" class="form-control validate" required />
+                                    <input id="name" name="product_quantity" type="text" placeholder="Product Quantity"
+                                        class="form-control validate" value="{{ old('product_quantity') }}" required />
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="category">Category</label>
-                                    <select class="custom-select tm-select-accounts" id="category">
+                                    <select class="custom-select tm-select-accounts" id="category_id"
+                                        value="{{ old('category_id') }}" name="category_id">
                                         <option selected>Select category</option>
                                         @if ($categories)
                                             @foreach ($categories as $category)
@@ -45,15 +52,32 @@
                             <div class="form-group mb-3">
                                 <label for="name">Product Price
                                 </label>
-                                <input type="number" id="name" name="name" type="text" class="form-control validate"
-                                    required />
+                                <input type="number" id="name" name="product_price" type="text"
+                                    class="form-control validate" placeholder="Product Price"
+                                    value="{{ old('product_price') }}" required />
+                            </div>
+                            <div class="form-group mb-3">
+                                <div>
+                                    <label>Product Status</label>
+                                </div>
+
+                                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                    <label class="btn bg-olive active">
+                                        <input type="radio" name="option" value="Active" id="option_b1" autocomplete="off"
+                                            checked> Active
+                                    </label>
+                                    <label class="btn bg-olive">
+                                        <input type="radio" name="option" value="Inactive" id="option_b2"
+                                            autocomplete="off"> Inactive
+                                    </label>
+                                </div>
                             </div>
                         </div>
                         <div class="col-xl-6 col-lg-6 col-md-12  mb-4">
                             <div class="form-group mb-3">
                                 <label for="name">
-                                    <input type="file" id="name" name="name" type="text" class="form-control validate"
-                                        required />
+                                    <input type="file" id="name" name="product_image" type="text"
+                                        class="form-control validate" required />
                                 </label>
 
                             </div>
@@ -62,6 +86,25 @@
                             <button type="submit" class="btn btn-primary btn-block text-uppercase">Add Product Now</button>
                         </div>
                         </form>
+                        @if ($errors->any())
+                            <div>
+                                @foreach ($errors->all() as $error)
+                                    <li class="alert alert-danger">{{ $error }}</li>
+                                @endforeach
+                            </div>
+                        @endif
+
+                        @if (\Session::has('error'))
+                            <div>
+                                <li class="alert alert-danger">{!! \Session::get('error') !!}</li>
+                            </div>
+                        @endif
+
+                        @if (\Session::has('success'))
+                            <div>
+                                <li class="alert alert-success">{!! \Session::get('success') !!}</li>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
